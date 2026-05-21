a3be72da9ea7a71e63297e89b53fbc246a6789ba
Dev Olusegun <12515139o@gmail.com>
2026-05-21 17:26:08 +0100

    adding exceptions handlers for possible errors at runtime

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler 
+{
+    public function register(): void
+    {
+        $this->renderable(function (Throwable $e, $request) {
+            if (! $request->expectsJson() && ! $request->is('api/*')){
+                return null;
+            }
+
+            //404 model not found
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json([
+                    'message' => 'Resource Not Found.',
+                ], 404);
+            }
+
+            // 404 — route not found
+            if ($e instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'Endpoint not found.',
+                ], 404);
+            }
+
+
+
+            return null;
+        });
+
+    }
+
+}
